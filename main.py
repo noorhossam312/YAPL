74b4bafbabdd1aa6f0aa1bae7f38409085a9ad11
Noor Hossam <noor.hossam312@gmail.com>
2026-03-18 13:12:19 +0200

    Add if conditions

--- main.py
+++ main.py
@@ -24,33 +24,71 @@ file = input(r"""
 
 path to file.ypl
 ╰─ """)
-
+if not file:
+    file = "tests\\hello_world.ypl"
 check(file)
 
 with open(file) as f:
     f_split = f.read().splitlines()
     env = {}
-    for line in f_split:
+    condition_tf = True
+    for line_num, line in enumerate(f_split):
+        line = line.lstrip()
         space_split = line.split(" ")
+        if not condition_tf:
+            if not line.startswith(";"):
+                continue
+            else:
+                condition_tf = True
+                continue
         if line.startswith("$"):
             v_name = line.split(" ")[0][1:]
             key = " ".join(line.split(" ")[2:])
             if '+' in key:
-                _key = str(eval(key))
-                key = _key
+                key = str(eval(key))
             elif '-' in key:
-                _key = str(eval(key))
-                key = _key
+                key = str(eval(key))
             elif '*' in key:
-                _key = str(eval(key))
-                key = _key
+                key = str(eval(key))
             elif '/' in key:
-                _key = str(eval(key))
-                key = _key
+                key = str(eval(key))
             env[v_name] = key
+            continue
         if line.startswith("print"):
             text = space_split[1:]
             for i in text:
                 if i.startswith("$"):
                     text[text.index(i)] = env[i[1:]]
             print(" ".join(text))
+            continue
+        if line.startswith("if"):
+            condition = line[2:-1]
+            if condition[0] == " ":
+                condition = condition[1:]
+            condition = condition[1:-1]
+            operator = condition.split(" ")[1]
+            var_a = env[condition.split(" ")[0][1:]]
+            var_b = env[condition.split(" ")[2][1:]]
+            if operator == "is":
+                try:
+                    var_a = float(var_a)
+                except ValueError:
+                    pass
+                try:
+                    var_b = float(var_b)
+                except ValueError:
+                    pass
+                if var_a != var_b:
+                    condition_tf = False
+            continue
+            # print(f"{var_a} {type(var_a)} {var_b} {type(var_b)}")  # debug
+            # print(operator)  # debug
+        if not line.startswith(";"):
+            raise SyntaxError(f"{RED}Invalid syntax on line number {line_num+1}.\n{line}{RESET}")
+
+
+
+"""ex:
+if($x):
+print 505
+;"""
